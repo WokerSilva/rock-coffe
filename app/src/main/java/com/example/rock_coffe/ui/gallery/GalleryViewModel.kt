5b88056740ccb80c50a212389c2a1e5911b8b849
WokerSilva <marcosilva@ciencias.unam.mx>
2025-04-27 07:48:14 -0600

    Iniciando aplicación

---
+++ app/src/main/java/com/example/rock_coffe/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.rock_coffe.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/example/rock_coffe/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.rock_coffe.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
